832da658907579f15a5a3bd7685fac2b2e971455
Abigail Charime <abigailcharime@Abigails-MacBook-Air.local>
2025-08-04 16:49:30 -0700

    flask and mongodb set up

---
+++ routes.py
@@ -0,0 +1,8 @@
+from flask import render_template
+from app import mongo
+
+@app.route("/")
+def home_page():
+    online_users = mongo.db.users.find({"online": True})
+    return render_template("index.html",
+        online_users=online_users)
