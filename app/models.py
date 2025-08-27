534258b4f74cb7e6f91b7476558222ae5927814c
Abigail Charime <abigailcharime@Abigails-MacBook-Air.local>
2025-08-26 19:38:15 -0700

    Add chat models and routes to get and create chats

--- app/models.py
+++ app/models.py
@@ -1,12 +1,18 @@
 from . import mongo
+from bson.objectid import ObjectId
+from typing import Optional 
 
 class User:
     @staticmethod
-    def create(username, email, password):
+    def create(username, email, password, date, avatarUrl, role):
         user = {
             'username': username,
             'email': email,
-            "password": password
+            "password": password,
+            "role": role,
+            "lastSeen": date,
+            "avatarUrl": avatarUrl
+
         }
         mongo.db.users.insert_one(user)
         return user
@@ -25,3 +31,26 @@ class User:
     @staticmethod
     def delete(username):
         mongo.db.users.delete_one({'username': username})
+
+class Chat:
+    @staticmethod
+    def create(name: Optional[str], ownerId, isGroup, members, createdAt, active):
+        chat = {
+              "name": name,
+              "ownerId": ownerId,
+              "isGroup": isGroup,       
+             "members": members,  
+              "createdAt": createdAt,
+              "active": active,
+        }
+        mongo.db.chats.insert_one(chat)
+        return chat
+    @staticmethod
+    def get_by_id(id):
+        # Get all members data in all chats
+        return mongo.db.chats.find_one({"_id": ObjectId(id)})
+
+    @staticmethod
+    def get_by_user_id(user_id):
+        # Get all members data in all chats
+        return mongo.db.chats.find({"members": int(user_id)})
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,13 @@
 from flask import Blueprint, request, jsonify
-from .models import User
+from .models import *
  
 main = Blueprint('main', __name__)
- 
+
+# Users Routes
 @main.route('/users', methods=['POST'])
 def create_user():
     data = request.json
-    user = User.create(data['username'], data['email'], data["password"])
+    user = User.create(data['username'], data['email'], data["password"], data['lastSeen'],data["avatarUrl"], data['role'],)
     return jsonify(user), 201
  
 @main.route('/users/<username>', methods=['GET'])
@@ -26,3 +27,30 @@ def update_user(username):
 def delete_user(username):
     User.delete(username)
     return jsonify({'message': 'User deleted'}), 200
+
+@main.route('/users/<user_id>/chats', methods=["GET"])
+def get_specific_users_chats(user_id):
+    chats = Chat.get_by_user_id(user_id)
+    if chats:
+        return jsonify(chats), 200
+    return jsonify({'error': "User not found"}), 404
+
+# Chats Routes
+@main.route('/chats', methods=["POST"])
+def create_chat():
+    data = request.json
+    print(data)
+    if "name" not in data:
+        data["name"] = None
+
+    chat = Chat.create(data['name'], data['ownerId'], data['isGroup'], data['members'], data["createdAt"],data['active'])
+    return jsonify(chat), 201
+
+@main.route('/chats/<id>', methods=["GET"])
+def get_chat_details(id):
+    chat = Chat.get_by_id(id)
+    if chat:
+        return jsonify(chat), 200
+    return jsonify({'error': "User not found"}), 404
+
+
