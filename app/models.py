e4d9fea2dec32e2f8f62b9829c60e2fbb5a322dc
Abigail Charime <abigailcharime@Abigails-MacBook-Air.local>
2025-08-28 21:04:16 -0700

    Added routes and methods to update memebers in chat and update certain fields in chat

--- app/models.py
+++ app/models.py
@@ -16,30 +16,40 @@ class User:
         }
         mongo.db.users.insert_one(user)
         return user
-
+    @staticmethod
+    def get_user(id):
+        return mongo.db.users.find_one({'_id': ObjectId(id)})
+    
     @staticmethod
     def get_by_username(username):
         return mongo.db.users.find_one({'username': username})
 
     @staticmethod
-    def update(username, new_email):
-        mongo.db.users.update_one(
-            {'username': username},
+    def update(id, new_email):
+        return mongo.db.users.update_one(
+            {'_id': ObjectId(id)},
             {'$set': {'email': new_email}}
         )
-
+    
     @staticmethod
-    def delete(username):
-        mongo.db.users.delete_one({'username': username})
-
+    def delete(id):
+        # chats = Chat.get_by_user_id(id)
+        # for chat in chats:
+        #     pass
+        #     # remove user from chats
+        return mongo.db.users.delete_onf({'_id': ObjectId(id)})
+    
 class Chat:
     @staticmethod
     def create(name: Optional[str], ownerId, isGroup, members, createdAt, active):
+        objectid_members = []
+        for m in members:
+            objectid_members.append(ObjectId(m))
         chat = {
               "name": name,
               "ownerId": ownerId,
               "isGroup": isGroup,       
-             "members": members,  
+             "members": objectid_members,  
               "createdAt": createdAt,
               "active": active,
         }
@@ -51,6 +61,41 @@ class Chat:
         return mongo.db.chats.find_one({"_id": ObjectId(id)})
 
     @staticmethod
-    def get_by_user_id(user_id):
-        # Get all members data in all chats
-        return mongo.db.chats.find({"members": int(user_id)})
+    def modify_fields(id,data):
+        update_operation = {}
+        if data:
+            update_operation['$set'] = data
+
+            return mongo.db.chats.update_one({"_id": ObjectId(id)}, update_operation)
+
+    @staticmethod
+    def delete_members(id, members):
+        if members:
+            deleted_mems = []
+            for m in members:
+                deleted_mems.append(ObjectId(m))
+            return mongo.db.chats.update_one({'_id': ObjectId(id)},{
+                '$pull': {
+                    'members':
+                    {'$in': deleted_mems}
+                    }
+                }
+            )     
+    
+    @staticmethod
+    def add_members(id, members):
+        if members:
+            added_mems = []
+            for m in members:
+                added_mems.append(ObjectId(m))
+            return mongo.db.chats.update_one({'_id': ObjectId(id)},{
+                '$addToSet': {
+                    'members':
+                    {'$each': added_mems}
+                    }
+                }
+            ) 
+
+    @staticmethod
+    def delete(id):
+        return mongo.db.chats.delete_one({"_id": ObjectId(id)})
--- app/routes.py
+++ app/routes.py
@@ -7,26 +7,47 @@ main = Blueprint('main', __name__)
 @main.route('/users', methods=['POST'])
 def create_user():
     data = request.json
-    user = User.create(data['username'], data['email'], data["password"], data['lastSeen'],data["avatarUrl"], data['role'],)
+    if not data.get('username'):
+        return jsonify({'error': 'Username field is required'},400)
+    elif not data.get('email'):
+        return jsonify({'error': 'Email field is required'}, 400)
+    elif not data.get('password'):
+        return jsonify({'error': 'Password field is required'}, 400)
+    role = data.get('role')
+
+    if not role:
+        role = "user"
+    elif role != "admin" and role != 'user':
+        return jsonify({'error': 'Role must either be "admin" or "user'}, 400)
+
+    user = User.create(data['username'], data['email'], data["password"], data['lastSeen'],data["avatarUrl"], role)
     return jsonify(user), 201
- 
-@main.route('/users/<username>', methods=['GET'])
-def get_user(username):
+
+@main.route('/users/<id>', methods=['GET'])
+def get_user(id):
+    user = User.get_user(id)
+    if user:
+        return jsonify(user), 200
+    return jsonify({'error': 'User with id: {id} was not found, this user may not exist. Please verify that this is a valid user id'}), 404
+
+@main.route('/users/usernames/<username>', methods=['GET'])
+def get_user_by_username(username):
     user = User.get_by_username(username)
     if user:
         return jsonify(user), 200
     return jsonify({'error': 'User not found'}), 404
  
-@main.route('/users/<username>', methods=['PUT'])
-def update_user(username):
+@main.route('/users/<id>', methods=['PUT'])
+def update_user(id):
     data = request.json
-    User.update(username, data['email'])
-    return jsonify({'message': 'User updated'}), 200
- 
-@main.route('/users/<username>', methods=['DELETE'])
-def delete_user(username):
-    User.delete(username)
-    return jsonify({'message': 'User deleted'}), 200
+    if data.get('email'):
+        updated_user = User.update(id, data['email'])
+    else:
+        return jsonify({'message': 'Email is a required field'})
+
+    if updated_user.modified_count:
+        return jsonify({'message': f'User with id: {id} was updated'}), 200
+    return jsonify({'message': f'User with id: {id} was not updated, please verify that this is a valid user id'})
 
 @main.route('/users/<user_id>/chats', methods=["GET"])
 def get_specific_users_chats(user_id):
@@ -35,12 +56,18 @@ def get_specific_users_chats(user_id):
         return jsonify(chats), 200
     return jsonify({'error': "User not found"}), 404
 
+@main.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    deleted_user = User.delete(id)
+    if deleted_user.deleted_count:
+        return jsonify({'message': f'User with id: {id} was deleted'}), 200
+    return jsonify({'message': f'User with id: {id} was not deleted, please check that this is a valid user id'})
+
 # Chats Routes
 @main.route('/chats', methods=["POST"])
 def create_chat():
     data = request.json
-    print(data)
-    if "name" not in data:
+    if not data.get('name'):
         data["name"] = None
 
     chat = Chat.create(data['name'], data['ownerId'], data['isGroup'], data['members'], data["createdAt"],data['active'])
@@ -51,6 +78,65 @@ def get_chat_details(id):
     chat = Chat.get_by_id(id)
     if chat:
         return jsonify(chat), 200
-    return jsonify({'error': "User not found"}), 404
+    return jsonify({'error': "Chat not found"}), 404
 
+@main.route("/chats/<id>", methods=["PATCH"])
+def update_chat(id):
+    data = request.json
+    modify_data = {}
+
+    for field in data:
+        if data.get(field):
+            modify_data[field] = data[field]
+        
+    modified_fields = Chat.modify_fields(id, modify_data)
+    modified_count = modified_fields.modified_count
+    if modified_count or modified_fields.matched_count:
+        message = ""
+        if modified_count > 0:
+            message = f"Chat {id} was updated"
+        else: 
+            message = f"Chat{id} was not updated, request data was identical to database"
+
+        return jsonify({'message': message}, 200)
+    
+    return jsonify({'message': f'There were issues updating chat {id}'}, 400)
+
+@main.route("/chats/<id>/members", methods=["PATCH"])
+def update_chat_members(id):
+    data = request.json
+    add = data.get('add')
+    delete = data.get('delete')
+
+    add_count, delete_count = 0,0
+    add_modified_count, delete_modified_count = 0,0
+    add_match_count, delete_match_count = 0,0
+    if add:
+        add_count = len(add)
+        added_res = Chat.add_members(id, add)
+        add_modified_count = added_res.modified_count
+        add_match_count = added_res.matched_count
+
+    if delete:
+        delete_count = len(delete)
+        deleted_res = Chat.delete_members(id, delete)
+        delete_modified_count = deleted_res.modified_count
+        delete_match_count = deleted_res.matched_count
+
+    add_success_message = None
+    delete_success_message = None
+    if add_count == add_modified_count or add_count == add_match_count:
+        add_success_message = f'{add_modified_count} member(s) were added'
+    if delete_count == delete_modified_count or delete_count == delete_match_count:
+        delete_success_message = f'{delete_modified_count} member(s) were deleted'
+    if add_success_message and delete_success_message:
+        return jsonify({'message':f'Chat {id}: {add_success_message} and {delete_success_message}.'}, 200)
+    return jsonify({'message': f'There were issues updating chat {id}. {add_modified_count}/{add_count} were added and {delete_modified_count}/{delete_count} were deleted'}, 400)
+
+@main.route("/chats/<id>", methods=["DELETE"])
+def delete_chat(id):
+    deleted_chat = Chat.delete(id)
+    if deleted_chat.deleted_count:
+        return jsonify({'message': f'Chat {id} was deleted'}), 200
+    return jsonify({'message': f'Chat {id} was not deleted, please check that this is a valid chat id'},400)
 
